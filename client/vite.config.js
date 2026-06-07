2fa01a4bd684c2fe2aebb1930535a1ed1f68c83c
Gage Billlinger <gagebillinger@icloud.com>
2026-06-06 19:32:33 -0500

    Add preview configuration to Vite for allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,11 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "barnbuddy.pro",
+      "www.barnbuddy.pro"
+    ]
+  }
 })
